658199ca2f5c0c189a29bad2ad8aac665e0f5d13
Xin <70026238+XinOnGithub@users.noreply.github.com>
2021-04-12 00:07:06 +0200

    Optimized script

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ tokens = open('tokens.txt','r').read().splitlines()
 
 def Spammer():
 	for token in tokens:
-		now = datetime.now()
-		time = now.strftime("%H:%M:%S")
+		time = datetime.now().strftime("%H:%M:%S")
 		rqs = requests.post(f'https://discordapp.com/api/v8/channels/{channel_id}/messages', json={"content": message,"tts": "false"}, headers={'authorization': token})
-		response_data = rqs.json()
 		if rqs.status_code == 200:
 			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Sended Message With Succes")
 		elif rqs.status_code == 401:
 			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}Token is invalid")
 		elif rqs.status_code == 403:
-			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}" + response_data["message"])
+			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}Eror")
 		else:
 			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}Something is wrong")
 
 def Joiner(invite):
 	for token in tokens:
-		now = datetime.now()
-		time = now.strftime("%H:%M:%S")
+		time = datetime.now().strftime("%H:%M:%S")
 		joiner_rqs = requests.post(f'https://discordapp.com/api/v8/invites/{invite}', headers={'authorization': token})
 		if joiner_rqs.status_code == 200:
 			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Succesfully Joined ")
+		elif rqs.status_code == 401:
+			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}Token is invalid")
 		elif joiner_rqs.status_code == 404:
 			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET} {Fore.RED}Unknow Invite ")
 		else:
@@ -35,23 +34,16 @@ def Joiner(invite):
 
 def Leaver(server_id):
 	for token in tokens:
-		now = datetime.now()
-		time = now.strftime("%H:%M:%S")
+		time = datetime.now().strftime("%H:%M:%S")
 		leaver_rqs = requests.delete(f'https://discord.com/api/v8/users/@me/guilds/{server_id}', headers={'authorization': token})
-		print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Succesfully Leaved")
-
-
-def Friender(name, discrim):
-	for token in tokens:
-		now = datetime.now()
-		time = now.strftime("%H:%M:%S")
-		Friender_rqs = requests.post('https://discordapp.com/api/v8/users/@me/relationships', headers = {'Authorization': token}, json = {'username':name,'discriminator':discrim})
-		print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Friend Requests Sended To {name}#{discrim}")
-
+		if leaver_rqs == 429 or 401:
+			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Eror")
+		else:
+			print(f"{Fore.RED}[{Fore.RESET}{Fore.LIGHTBLACK_EX}{time}{Fore.RED}]{Fore.RESET}{Fore.GREEN}Succesfully Leaved")
 
 def menu():
 	mennu = """
-\t\t\t      [1] Spammer [2] Joiner [3] Leaver  [4] Friender
+\t\t\t            [1] Spammer [2] Joiner [3] Leaver  
 	"""
 	print(f"{Fore.LIGHTBLACK_EX}"+mennu)
 
@@ -62,34 +54,32 @@ def banner():
 \t\t\t\t ║║║╚═╗║  ║ ║╠╦╝ ║║  ╔╩╦╝  ╠╦╝╠═╣║ ║║║╣ ╠╦╝
 \t\t\t\t═╩╝╩╚═╝╚═╝╚═╝╩╚══╩╝  ╩ ╚═  ╩╚═╩ ╩╩═╩╝╚═╝╩╚═"""
 	print(f"{Fore.LIGHTCYAN_EX}{bannner}")
-banner()
-menu()
-choose = int(input(">"))
-os.system("cls")
-banner()
-menu()
-if choose == 1:
-    channel_id = int(input("Channel id : "))
-    message = str(input("Message : "))
-    amount = int(input("How many thead : "))
-    os.system("cls")
-    banner()
-    menu()
-    print("")
-    for i in range(amount):
-        t = threading.Thread(target=Spammer)
-        threads.append(t)
-        t.start()
-elif choose == 2:
-    invite = str(input("Invite link : "))
-    Joiner(invite)
-elif choose == 3:
-    server_id = str(input("Server id : "))
-    Leaver(server_id)
-    print("All tokens leaved server")
-elif choose == 4:
-	name = str(input("Username : "))
-	discrim = str(input("Discriminator : "))
-	Friender(name, discrim)
-else:
-    print("error")
+
+
+while True:
+	banner()
+	menu()
+	choose = int(input(">"))
+	banner()
+	menu()
+	if choose == 1:
+	    channel_id = int(input("Channel id : "))
+	    message = str(input("Message : "))
+	    amount = int(input("How many thead : "))
+	    os.system("cls")
+	    banner()
+	    menu()
+	    print("")
+	    for i in range(amount):
+	        t = threading.Thread(target=Spammer)
+	        threads.append(t)
+	        t.start()
+	elif choose == 2:
+	    invite = str(input("Invite link : "))
+	    Joiner(invite)
+	elif choose == 3:
+	    server_id = str(input("Server id : "))
+	    Leaver(server_id)
+	    print("All tokens leaved server")
+	else:
+	    print("error")
